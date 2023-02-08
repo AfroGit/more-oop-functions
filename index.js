501ea660c4ce2bb1e5185d9e4c963539facdf21b
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-02-08 04:49:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Two functions – one object
+// importance: 2
+
+// Is it possible to create functions A and B so that new A() == new B()?
+let obj = {}
+ function A() { return obj; }
+ function B() { return obj; }
+
+ let a = new A();
+ let b = new B();
+
+ console.log( a == b ); // true
+
+// If it is, then provide an example of their code.
