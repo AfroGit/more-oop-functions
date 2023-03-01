f64b5a94c64af69866fdbaf2f1dec0fe8b978194
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-01 01:22:02 +0000

    Accessing Nested Arrays

--- index.js
+++ index.js
@@ -52,6 +52,8 @@
 
 
 /*Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object. */ 
+
+
 const myPlants = [
   {
     type: "flowers",
